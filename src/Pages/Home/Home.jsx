988ede35207938f8907322f63295cb90a07d2006
Dev Pandey <139095228+dpandey62@users.noreply.github.com>
2026-03-02 23:27:47 +0530

    Refactor Home component layout and styling
    Updated Home component structure and styles.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,65 +1,4 @@
- //import React from "react";
 
-// // Home page sections
-// import FancyBanner from  "../../Components/FancyBanner/FancyBanner";
-// import AboutClub from "../../Components/AboutClub/AboutClub";
-// import Facilities from "../../Components/Facilities/Facilities";
-// import PopularCourts from "../../Components/PopularCourts/PopularCourts";
-// import Promotions from "../../Components/Promotions/Promotions";
-// import Testimonials from "../../Components/Testimonials/Testimonials";
-
-//  import Location from "../../Components/Location/Location";
-// import Newsletter from "../../Components/Newsletter/Newsletter";
-
-// const Home = () => {
-//   return (
-//     <main className="w-full overflow-x-hidden">
-
-//       {/* Hero Section */}
-//       <section className="mb-16">
-//         <FancyBanner />
-//       </section>
-
-//       {/* About Club */}
-//       <section className="mb-16">
-//          <AboutClub />
-//       </section>
-
-//       {/* Facilities */}
-//       <section className="mb-16 bg-gray-50 py-10">
-//         <Facilities />
-//       </section>
-
-//       {/* Popular Courts */}
-//       <section className="mb-16">
-//         <PopularCourts />
-//       </section>
-
-//       {/* Promotions */}
-//       <section className="mb-16 bg-primary/5 py-10">
-//         <Promotions />
-//       </section>
-
-//       {/* Testimonials */}
-//       <section className="mb-16">
-//         <Testimonials />
-//       </section>
-
-      //  {/* Location */}
-      // <section className="mb-16">
-      //    <Location />
-      //  </section>
-
-//       {/* Newsletter */}
-//       <section className="mb-20">
-//         <Newsletter />
-//       </section>
-
-//     </main>
-//   );
-// };
-
-// export default Home;
 
 import FancyBanner from "../../Components/FancyBanner/FancyBanner";
 import AboutClub from "../../Components/AboutClub/AboutClub";
@@ -69,24 +8,24 @@ import Newsletter from "../../Components/Newsletter/Newsletter";
 
 const Home = () => {
   return (
-    <main className="w-full overflow-x-hidden">
+     <main className="w-full overflow-x-hidden bg-black space-y-20">
       <section className="mb-20">
         <FancyBanner />
       </section>
 
-         <section className=" mb-20">
+         <section >
         <AboutClub />
       </section>
 
-      <section className="mb-20 bg-gray-50 py-16">
+      <section >
         <Facilities />
       </section>
 
-      <section className="mb-20">
+      <section >
         <Location />
       </section>
 
-      <section className="mb-24">
+      <section >
         <Newsletter />
       </section>
       
